cfb5dd301206f931b2cc79abb2ef2da4e9146da5
Noah8922 <juyeolgo@gmail.com>
2022-07-26 14:12:13 +0900

    Type inference

--- app.ts
+++ app.ts
@@ -1,11 +1,18 @@
-/* Core Type - number */
-function add(n1: number, n2: number) {
-  if (typeof n1 !== 'number' )
-  return n1 + n2;
+/* Core Type - number, string, boolen */
+function add(n1: number, n2: number, showResult: boolean, resultPhrase : string) {
+  const result = n1 + n2
+  if (showResult) {
+    console.log(resultPhrase + result ); // Result is : 7.8
+    //console.log(resultPhrase + n1 + n2); // Result is : 52.8
+  } else {
+    return n1 + n2;
+  }
 }
 
-const number1 = 5;
+// const number1 = 5;
+let number1 = 5
 const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result is : '
 
-const result = add(number1, number2);
-console.log(result);
+add(number1, number2, printResult, resultPhrase);
